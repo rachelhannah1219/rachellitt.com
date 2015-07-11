22381da5df3f05f4cb8670f7abdfe17a2e3a7136
Rachel Litt <rachel.litt@gmail.com>
2015-07-11 16:53:54 -0700

    making sure we dont lose work

--- plugins/blog.py
+++ plugins/blog.py
@@ -34,7 +34,7 @@ def preBuild(site):
 			context_post = {"path": page.path}
 
 			# Check if we have the required keys
-			for field in ["title", "date", "tags", "thumbnail"]:
+			for field in ["title", "date", "tags", "thumbnail", "color"]:
 				
 				if not context.has_key(field):
 					logging.warning("Page %s is missing field: %s" % (page.path, field))
